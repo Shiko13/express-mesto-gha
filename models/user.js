82f5d8f939f14daccd04a24177a3f494f9369166
Sergey <shikofreid@yandex.ru>
2023-07-05 19:52:40 +0400

    Added validator

--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    minLength: 2,
-    maxLength: 30,
-    required: true,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" — 2'],
+    maxlength: [30, 'Максимальная длина поля "name" — 30'],
   },
   about: {
     type: String,
@@ -16,6 +17,10 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    validate: {
+      validator: (v) => validator.isURL(v),
+      message: 'Некорректный URL',
+    },
   },
 });
 
