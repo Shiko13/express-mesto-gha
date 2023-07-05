d68fd287e7e8b83c6da713f98ec679252d0a4248
Sergey <shikofreid@yandex.ru>
2023-07-05 19:57:10 +0400

    Some small improves

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 }).then(() => {
   console.log('Connected to database "mestodb"');
